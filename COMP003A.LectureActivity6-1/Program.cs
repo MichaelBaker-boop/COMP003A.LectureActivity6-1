3104098aee4be7f9bebcc768fd98523fd293d5e9
Michael Baker <michaelbaker4@my.whccd.edu>
2025-02-26 21:23:39 -0800

    Added usage of classes

--- COMP003A.LectureActivity6-1/Program.cs
+++ COMP003A.LectureActivity6-1/Program.cs
@@ -2,9 +2,17 @@
 {
     internal class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            
+            // Create a Vehicle object with brand and year
+            Vehicle myVehicle = new Vehicle { Brand = "Toyota", Year = 2020 };
+            // Create a Car object with brand, year, and number of doors
+            Car myCar = new Car("Honda", 2022, 4);
+
+            // Display vehicle details
+            myVehicle.DisplayInfo();
+            // Display car details
+            myCar.DisplayInfo();
         }
     }
 }
